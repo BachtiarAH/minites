b3ae53c9bcc8585215944c6d64a4a5dee64aacf7
BachtiarAH <bachtiarah73@gmail.com>
2023-05-15 23:43:36 +0700

    [add] service for penulis

--- app/Http/Service/PenulisService.php
+++ app/Http/Service/PenulisService.php
@@ -9,4 +9,40 @@ class PenulisService extends Service{
     public function __construct() {
         $this->penulis = new PenulisModel();
     }
+
+    public function create($username, $password)
+    {
+        return $this->penulis->create([
+            'username'=>$username,
+            'password'=>$password
+        ]);
+    }
+
+    public function findAll()
+    {
+        return $this->penulis->get();
+    }
+
+    public function findByUsername($username)
+    {
+        return $this->penulis->where('username',$username)->first();
+    }
+
+    public function update($id, $username, $password)
+    {
+        return $this->penulis->find($id)->update([
+            'username'=>$username,
+            'password'=>$password
+        ]);
+    }
+
+    public function delete($id)
+    {
+        return $this->penulis->find($id)->delete();
+    }
+
+    function findById($id)
+    {
+        return $this->penulis->find($id);
+    }
 }
